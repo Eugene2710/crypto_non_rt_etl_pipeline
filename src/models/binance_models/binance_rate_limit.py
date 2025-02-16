38910318806de37d62d08182f65d4620f243365b
Gene <39414608+Eugene2710@users.noreply.github.com>
2025-02-16 11:49:00 +0800

    Update src/models/binance_models/binance_rate_limit.py
    Co-authored-by: writebugstogetherstrong <writermonkey27@gmail.com>

--- src/models/binance_models/binance_rate_limit.py
+++ src/models/binance_models/binance_rate_limit.py
@@ -2,9 +2,11 @@ from typing import Literal
 from pydantic import BaseModel
 
 
-# rate limit model
-# https://developers.binance.com/docs/binance-spot-api-docs/enums#rate-limiters-ratelimittype
 class RateLimit(BaseModel):
+    """
+    Rate Limit Model
+    - https://developers.binance.com/docs/binance-spot-api-docs/enums#rate-limiters-ratelimittype
+    """
     rateLimitType: Literal["REQUEST_WEIGHT", "ORDERS", "RAW_REQUESTS"]
     interval: str
     intervalNum: int
