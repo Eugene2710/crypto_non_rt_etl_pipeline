ddf5216fd3e364a270ed67978a9e674e3b6b1e54
eugeneleejunping <eugeneleejunping@gmail.com>
2026-04-03 21:41:08 +0800

    Edited names of dto for clarity

--- src/dao/s3_import_status_dao.py
+++ src/dao/s3_import_status_dao.py
@@ -19,7 +19,7 @@ from datetime import datetime
 import logging
 
 from database_management.chainstack.tables import s3_import_status_table
-from src.models.database_transfer_objects.s3_import_status import S3ImportStatusDTO
+from src.models.database_transfer_objects.s3_import_status import S3ToDBImportStatusDTO
 from src.utils.logging_utils import setup_logging
 
 logger = logging.getLogger(__name__)
@@ -37,7 +37,7 @@ class S3ImportStatusDAO:
         reraise=True
     )
     async def insert_latest_import_status(
-        self, import_status: S3ImportStatusDTO, conn: AsyncConnection
+        self, import_status: S3ToDBImportStatusDTO, conn: AsyncConnection
     ) -> None:
         insert_text_clause: Insert = insert(self._table).values(
             import_status.model_dump()
@@ -73,7 +73,7 @@ if __name__ == "__main__":
     load_dotenv()
     connection_string: str = os.getenv("CHAIN_STACK_PG_CONNECTION_STRING", "")
     dao: S3ImportStatusDAO = S3ImportStatusDAO(connection_string)
-    s3_import_status_dto: S3ImportStatusDTO = S3ImportStatusDTO(
+    s3_import_status_dto: S3ToDBImportStatusDTO = S3ToDBImportStatusDTO(
         data_source="chainstack_eth_blocks",
         file_modified_date=datetime.utcnow(),
         created_at=datetime.utcnow(),
