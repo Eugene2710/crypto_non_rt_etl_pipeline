ff6214be352a411530476dd292c86610acc654c3
eugeneleejunping <eugeneleejunping@gmail.com>
2026-04-03 19:21:17 +0800

    Renamed S3ImportStatusDTO to S3ToDBImportStatusDTO for clarity

--- src/models/database_transfer_objects/s3_import_status.py
+++ src/models/database_transfer_objects/s3_import_status.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 from datetime import datetime
 
 
-class S3ImportStatusDTO(BaseModel):
+class S3ToDBImportStatusDTO(BaseModel):
     data_source: str
     file_modified_date: datetime
     created_at: datetime
